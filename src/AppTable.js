ebe5838df7c3cb224c6f97bb8fbe8866a0c315ea
hzzhang <monsttrod@gmail.com>
2020-08-15 15:10:44 +0800

    complete table structure

--- src/AppTable.js
+++ src/AppTable.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import { Table } from 'antd';
+import { BarChartOutlined } from '@ant-design/icons';
+
+import { useDispatch } from 'react-redux';
+import { changeToDistrict, changeToProduct } from './actions';
 
 const { Column } = Table;
 
-// const data = [
-//   {
-//     key: '1',
-//     firstName: 'John',
-//     lastName: 'Brown',
-//     age: 32,
-//     address: 'New York No. 1 Lake Park',
-//     tags: ['nice', 'developer'],
-//   },
-//   {
-//     key: '2',
-//     firstName: 'Jim',
-//     lastName: 'Green',
-//     age: 42,
-//     address: 'London No. 1 Lake Park',
-//     tags: ['loser'],
-//   },
-//   {
-//     key: '3',
-//     firstName: 'Joe',
-//     lastName: 'Black',
-//     age: 32,
-//     address: 'Sidney No. 1 Lake Park',
-//     tags: ['cool', 'teacher'],
-//   },
-// ];
-
 export const AppTable = (props) => {
-  const data = props.products;
-  console.log(data);
+  let data = props.products;
+  data.push({key: "dis0", name: "地区"});
+  
+  const dispatch = useDispatch();
 
   return (
     <div>
@@ -43,7 +22,19 @@ export const AppTable = (props) => {
           key="firstName"
           render={name => {
             if (name === "产品") {
-              return <strong>{name}</strong>;
+              return (
+                <strong>
+                  {name} <BarChartOutlined onClick={() => dispatch(changeToProduct())}/>
+                </strong>
+              );
+            }
+
+            if (name === "地区") {
+              return (
+                <strong>
+                  {name} <BarChartOutlined onClick={() => dispatch(changeToDistrict())}/>
+                </strong>
+              );
             }
 
             return name;
@@ -54,14 +45,6 @@ export const AppTable = (props) => {
         <Column title="2019Q3" dataIndex="2019Q3" key="2019Q3" />
         <Column title="2019Q4" dataIndex="2019Q4" key="2019Q4" />
         <Column title="2020Q1" dataIndex="2020Q1" key="2020Q1" />
-      </Table>
-      <Table dataSource={data} pagination={false}>
-        <Column title="Main Body" dataIndex="firstName" key="firstName" />
-        <Column title="2019Q1" dataIndex="lastName" key="lastName" />
-        <Column title="2019Q2" dataIndex="age" key="age" />
-        <Column title="2019Q3" dataIndex="address" key="address" />
-        <Column title="2019Q4" dataIndex="address" key="address" />
-        <Column title="2020Q1" dataIndex="address" key="address" />
       </Table>
     </div>
   );
