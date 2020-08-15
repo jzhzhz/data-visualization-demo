799339c8ddce120af0b416d7b64d6a5f1faa60fe
hzzhang <monsttrod@gmail.com>
2020-08-15 14:56:16 +0800

    change dummy data

--- src/Data.js
+++ src/Data.js
@@ -1,111 +1,125 @@
-export const Product1 = [
+export const Products = [
   {
     id: 101,
+    name: "ProductA",
     date: "2019Q1",
-    ratio: 28
+    ratio: 29.6,
   },
   {
     id: 102,
+    name: "ProductA",
     date: "2019Q2",
-    ratio: 34
+    ratio: 29.6,
   },
   {
     id: 103,
+    name: "ProductA",
     date: "2019Q3",
-    ratio: 16
+    ratio: 29.6,
   },
   {
     id: 104,
+    name: "ProductA",
     date: "2019Q4",
-    ratio: 28
+    ratio: 29.6,
   },
   {
     id: 105,
+    name: "ProductA",
     date: "2020Q1",
-    ratio: 28
+    ratio: 29.6,
   },
-];
-
-export const Product2 = [
   {
     id: 201,
+    name: "ProductB",
     date: "2019Q1",
-    ratio: 28
+    ratio: 33.4,
   },
   {
     id: 202,
+    name: "ProductB",
     date: "2019Q2",
-    ratio: 34
+    ratio: 33.4,
   },
   {
     id: 203,
+    name: "ProductB",
     date: "2019Q3",
-    ratio: 34
+    ratio: 33.4,
   },
   {
     id: 204,
+    name: "ProductB",
     date: "2019Q4",
-    ratio: 34
+    ratio: 33.4,
   },
   {
     id: 205,
+    name: "ProductB",
     date: "2020Q1",
-    ratio: 34
+    ratio: 33.4,
   },
 ];
 
-export const Beijing = [
+export const Districts = [
   {
     id: 301,
+    name: "Shanghai",
     date: "2019Q1",
-    ratio: 11
+    ratio: 18.4,
   },
   {
     id: 302,
+    name: "Shanghai",
     date: "2019Q2",
-    ratio: 11
+    ratio: 18.4,
   },
   {
     id: 303,
+    name: "Shanghai",
     date: "2019Q3",
-    ratio: 11
+    ratio: 18.4,
   },
   {
     id: 304,
+    name: "Shanghai",
     date: "2019Q4",
-    ratio: 11
+    ratio: 18.4,
   },
   {
     id: 305,
+    name: "Shanghai",
     date: "2020Q1",
-    ratio: 11
+    ratio: 18.4,
   },
-];
-
-export const Shanghai = [
   {
     id: 401,
+    name: "Beijing",
     date: "2019Q1",
-    ratio: 44
+    ratio: 51.2,
   },
   {
     id: 402,
+    name: "Beijing",
     date: "2019Q2",
-    ratio: 44
+    ratio: 51.2,
   },
   {
     id: 403,
+    name: "Beijing",
     date: "2019Q3",
-    ratio: 44
+    ratio: 51.2,
   },
   {
     id: 404,
+    name: "Beijing",
     date: "2019Q4",
-    ratio: 44
+    ratio: 51.2,
   },
   {
     id: 405,
+    name: "Beijing",
     date: "2020Q1",
-    ratio: 44
+    ratio: 51.2,
   },
 ];
